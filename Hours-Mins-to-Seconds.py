01e7b2b5b34f28fb11d5d83e868c80e32f119fe7
unknown <dharaneeshram@gmail.com>
2021-10-15 10:00:23 +0530

    Modified README and Source code
    Cleaned the source code

--- Hours-Mins-to-Seconds.py
+++ Hours-Mins-to-Seconds.py
@@ -6,14 +6,17 @@ window = Tk()
 window.title("Time Converter")
 window.geometry("200x150")
 
+# Variables to store user entered hours and minutes
 hours = IntVar()
 minutes = IntVar()
 
+# This function is called when the butten is clicked
 def calculate():
     s = (hours.get() * 3600) + (minutes.get() * 60)
     seconds = Label(window, text = str(s) + " Seconds")
     seconds.grid(row = 4, column = 1)
 
+# To display the required widgets 
 def display():
     hourLabel = Label(window, text = "Hours : ")
     hoursSpin = Spinbox(window, from_= 0, to = 24, textvariable = hours)
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tkinter-Python-GUI
-GUI using Tkinter
+Creating a simple Hours-Mins to Seconds converter with GUI using python [Tkinter](https://docs.python.org/3/library/tkinter.html) module.
 
-## Hours-Minutes to Seconds
-
-Python
+## To install Tkinter
+You need latest version of Python to use pip installer. Type the below text in your terminal.
+`pip install tkinter`
